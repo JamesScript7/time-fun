8493c6210307752311e3e0a1ffacae767aae56bb
James Kim <james.kim7@yahoo.com>
2017-04-01 17:54:04 -0400

    Completed Exercise 2
    - Created a date Object with the date of "1 January 2017"

--- date-object-exercises.js
+++ date-object-exercises.js
@@ -5,10 +5,19 @@ let
 	minutes = seconds * 60,
 	hours = minutes * 60,
 	days = hours * 24,
-	years = days * 365;
+	years = days * 365,
+	calendarMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 // console.log(`milliseconds in a year: ${years}`);
 
 
 // EXERCISE 1: Create a Date object with today's date.
 let date = new Date();
-console.log(date);
+
+// console.log(`Today's Date: ${date}`);
+console.log(`${calendarMonths[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`);
+
+
+// EXERCISE 2: Create a Date object with the date of 1 January 2017.
+let janDate = new Date(2017, 0);
+
+console.log(`${janDate.getDate()} ${calendarMonths[janDate.getMonth()]} ${janDate.getFullYear()}`);
