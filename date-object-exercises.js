0f447284946fa8acd805a9a061bc70a366bf023b
James Kim <james.kim7@yahoo.com>
2017-04-01 17:25:30 -0400

    Completed Exercise 1
    - Created a date object with today's date

--- date-object-exercises.js
+++ date-object-exercises.js
@@ -1,11 +1,14 @@
 'use strict';
 
 let
-	milliseconds = 1000,
-	seconds = 60,
-	minutes = milliseconds * seconds,
+	seconds = 1000,
+	minutes = seconds * 60,
 	hours = minutes * 60,
 	days = hours * 24,
 	years = days * 365;
+// console.log(`milliseconds in a year: ${years}`);
 
-console.log(`milliseconds in a year: ${years}`);
+
+// EXERCISE 1: Create a Date object with today's date.
+let date = new Date();
+console.log(date);
