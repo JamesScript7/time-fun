c546f129928168db696bf7cf06261b839cc861de
James Kim <james.kim7@yahoo.com>
2017-04-01 17:09:19 -0400

    Added millisecond conversions
    You are able to see these in milliseconds:
    - minutes
    - hours
    - days
    - years

---
+++ date-object-exercises.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let
+	milliseconds = 1000,
+	seconds = 60,
+	minutes = milliseconds * seconds,
+	hours = minutes * 60,
+	days = hours * 24,
+	years = days * 365;
+
+console.log(`milliseconds in a year: ${years}`);
