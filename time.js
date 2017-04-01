6b5b4a6ac8d5fa04821ea0f6d5aa096361c0543d
James Kim <james.kim7@yahoo.com>
2017-04-01 16:51:11 -0400

    First commit

---
+++ time.js
@@ -0,0 +1,11 @@
+var d = new Date();
+
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear());
+console.log(d.getHours());
+
+console.log(d.toLocaleString());
+console.log(d.toJSON());
+
+console.log(d.getMilliseconds() * d.getMilliseconds());
