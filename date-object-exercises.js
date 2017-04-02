46f3758dd5f4f3699abf5e5b3cfdaf4553a8bfa7
James Kim <james.kim7@yahoo.com>
2017-04-01 20:14:02 -0400

    Got to push each day from Jan 1st to current day into an array
    For the next commit:
    - Complete exercise 4
    - Need to make it into a object
    - Value needs to a a boolean

--- date-object-exercises.js
+++ date-object-exercises.js
@@ -6,24 +6,50 @@ let
 	hours = minutes * 60,
 	days = hours * 24,
 	years = days * 365,
-	calendarMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-// console.log(`milliseconds in a year: ${years}`);
-
+	calMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	date = new Date(),
+	janDate = new Date(2017, 0),
+	millisecondsSinceJan = date - janDate;
 
 // EXERCISE 1: Create a Date object with today's date.
-let date = new Date();
-
 // console.log(`Today's Date: ${date}`);
-console.log(`${calendarMonths[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`);
+console.log(`Todays's Date: ${calMonths[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`);
 
 
 // EXERCISE 2: Create a Date object with the date of 1 January 2017.
-let janDate = new Date(2017, 0);
+console.log(`${janDate.getDate()} ${calMonths[janDate.getMonth()]} ${janDate.getFullYear()}`);
 
-console.log(`${janDate.getDate()} ${calendarMonths[janDate.getMonth()]} ${janDate.getFullYear()}`);
 
 // EXERCISE 3: Find the number of days between today and 1 Jan 2017.
+let
+	daysSinceJan = Math.floor(millisecondsSinceJan / days);
 
-console.log(`The number of days since January 1st, 2017: ${Math.floor(
-	(date - janDate) / days
-)} days`);
+console.log(`The number of days since January 1st, 2017: ${daysSinceJan} days`);
+
+
+// EXERCISE 4: Create an array of Date objects from our start date to our end date.
+let
+	start = date.getTime() - millisecondsSinceJan,
+	end = date.getTime(),
+	dayArr = [];
+
+for(let i = start; i < end; i += days) {
+  dayArr.push(i);
+}
+
+for(let j=0; j < dayArr.length; j++) {
+  console.log(new Date(dayArr[j]));
+}
+
+
+
+
+
+
+
+
+// Exercise 5: Randomly set a boolean workout property on each date object in our array
+
+// Exercise 6: Did you workout on the first day of the year?
+
+// Exercise 7: Did you workout seven days ago?
